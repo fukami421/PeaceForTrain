492e159d67d19819696e318ad4e1a6385b340a5b
Ryuichi Fukami <fukaryu421@gmail.com>
2019-11-13 00:31:21 +0900

    added rxswift cocoa

--- PeaceForTrain/Views/ExchangeHomeViewController.swift
+++ PeaceForTrain/Views/ExchangeHomeViewController.swift
@@ -7,23 +7,37 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ExchangeHomeViewController: UIViewController {
+    let disposeBag = DisposeBag()
     var connectingVC: TryConnectViewController!
-
+    
+    @IBOutlet weak var getBtn: UIButton!
+    @IBOutlet weak var giveBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.bind()
         self.title = "席交換"
     }
-    @IBAction func getBtn(_ sender: Any) {
-        self.connectingVC = TryConnectViewController.init(nibName: nil, bundle: nil)
-        connectingVC.isGive = false
-        self.navigationController?.pushViewController(self.connectingVC, animated: true)
-    }
     
-    @IBAction func giveBtn(_ sender: Any) {
-        self.connectingVC = TryConnectViewController.init(nibName: nil, bundle: nil)
-        connectingVC.isGive = true
-        self.navigationController?.pushViewController(self.connectingVC, animated: true)
+    func bind()
+    {
+        self.getBtn.rx.tap
+        .subscribe { [unowned self] _ in
+            self.connectingVC = TryConnectViewController.init(nibName: nil, bundle: nil)
+            self.connectingVC.isGive = false
+            self.navigationController?.pushViewController(self.connectingVC, animated: true)
+        }
+        .disposed(by: self.disposeBag)
+        
+        self.giveBtn.rx.tap
+        .subscribe { [unowned self] _ in
+            self.connectingVC = TryConnectViewController.init(nibName: nil, bundle: nil)
+            self.connectingVC.isGive = true
+            self.navigationController?.pushViewController(self.connectingVC, animated: true)
+        }
+        .disposed(by: self.disposeBag)
     }
 }
